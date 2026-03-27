6fe81e0cd290833d39deca252e04eaa0235a5fcf
Mr Daro <info@timberlinepl.co.uk>
2026-03-27 12:46:07 +0000

    service pages added

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -3,26 +3,18 @@ export default function sitemap() {
     {
       url: "https://timberlinepl.co.uk",
       lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 1,
     },
     {
       url: "https://timberlinepl.co.uk/fencing-plymouth",
       lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.9,
     },
     {
       url: "https://timberlinepl.co.uk/decking-plymouth",
       lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.9,
     },
     {
       url: "https://timberlinepl.co.uk/gates-plymouth",
       lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.9,
     },
   ];
 }
